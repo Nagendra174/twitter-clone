e478c9e2d58b625d7b34e6e758c113a9a5aaf099
Nagendra174 <bejugamnagendra2002@gmail.com>
2025-06-13 00:51:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
+
+import path from "path";
 import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
-import path from "path";
-import { fileURLToPath } from 'url';
 
 import authRoutes from "./routes/auth.router.js";
 import userRoutes from "./routes/user.router.js";
 import postRoutes from "./routes/post.route.js";
 import notificationRoutes from "./routes/notification.route.js";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+
+import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
@@ -18,13 +19,15 @@ cloudinary.config({
 	api_key: process.env.CLOUDINARY_API_KEY,
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+
 const app = express();
-const PORT = 5000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
@@ -32,23 +35,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-
-
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
-
-mongoose.connect("mongodb://localhost:27017/twitter_clone")
-    .then(() => {
-        console.log("MongoDB connected");
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        });
-    })
-    .catch((err) => {
-        console.error("Failed to connect to MongoDB", err);
-    });
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
+	connectMongoDB();
+});
